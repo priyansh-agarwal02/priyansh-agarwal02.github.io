4c5ca53bbcc988d553894555432eb17a232d9a91
Priyansh Agarwal <103555624+priyansh-agarwal02@users.noreply.github.com>
2025-04-07 01:45:47 +0530

    Deploy site

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@react-three/drei'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Root path for GitHub Pages user site
   plugins: [react()],
-  base: '/portfolio-website/',
-}) 
+})
